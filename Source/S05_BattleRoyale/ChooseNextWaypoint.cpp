f5596fdb2580e78236ef913e45d533626c6a6861
toastmuseum <Deolijus@gmail.com>
2017-07-06 14:15:12 -0400

    Reading Blackboard data in C++ - Reading from and setting to black board values and displaying them to the output log
    ChooseNextWaypoint.h:
    - create a struct FBlackboardKeySelector IndexKey;
    - make it protected so that children of class can still access it
    - Make it an editanywhere uproperty
    - This exposes the IndexKey variable under Blackboard section in  the blueprint editor ChooseNextWaypoint Node Details panel
    
    ChooseNextWaypoint.cpp:
    - Create a BlackboardComponent BlackBoardComp variable that is accessed from the OwnerComp parameter
    - Needed to #include the BlackboardComponent (#include "BehaviorTree/BlackboardComponent.h") so that we can access information from the BlackboardComp
    - Create an Index variable to store the index value of the Blackboard key selector  "IndexKey"
    *        Get the index value from BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName)
    *        The "GetValueAsInt" is similar to the blueprint version "GetBlackboardValueAsInt". It takes in the IndexKey.SelectedKeyName and returns the index value
    
    Figuring out what to #Include:
    - Since the BlackboardComp variable is of type BlackboardComponent
    - Search for blackboard in Engine solution files
    - Look in Runtime Modules folder
    -  Notice that blackboardcomponent.h exists
    - be sure to use the direct parent folder and forward slash in the include
    - #include "BehaviorTree/BlackboardComponent.h"

--- Source/S05_BattleRoyale/ChooseNextWaypoint.cpp
+++ Source/S05_BattleRoyale/ChooseNextWaypoint.cpp
@@ -2,13 +2,15 @@
 
 #include "S05_BattleRoyale.h"
 #include "ChooseNextWaypoint.h"
-
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
 
-
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++ success"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	auto IndexName = IndexKey.SelectedKeyName;
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Name: %s -- Waypoint index: %i"), *(IndexName.ToString()), Index);
 	
 	return EBTNodeResult::Succeeded;
 
--- Source/S05_BattleRoyale/ChooseNextWaypoint.h
+++ Source/S05_BattleRoyale/ChooseNextWaypoint.h
@@ -16,5 +16,9 @@ class S05_BATTLEROYALE_API UChooseNextWaypoint : public UBTTaskNode
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
 	
 };
